5b8f63e6783b34582ae688fb8a09535f572fe1ed
Kenneth Fechter <kenneth.fechter@gmail.com>
2015-07-18 16:09:58 -0400

    Working AWS S3 code
    whooop

--- controllers/fileupload.js
+++ controllers/fileupload.js
@@ -2,12 +2,26 @@ var AWS = require('aws-sdk');
 var fs = require('fs');
 
 AWS.config.loadFromPath('../config/AWSConfig.json');
+AWS.config.update({region: "", endpoint : "https://s3.amazonaws.com" });
+uploadFileToAWS("C:\\Github\\Test.txt", "kfechter-testFile.txt");
+getFileFromAws("kfechter-testFile.txt", "C:\\Github\\TestDownload.txt");
 
 
-function uploadFileToAWS(filepath, filename){
-    var body = fs.createReadStream(filepath);
-    var s3obj = new AWS.S3({ params: { Bucket: '3d-plasticity', Key: filename } });
-    s3obj.upload({Body : body}).
-        on('httpUploadProgress', function (evt) { console.log(evt);}).
-        send(function (err, data) { console.log(err,data)});
+function uploadFileToAWS(filepath, filename) {
+
+    var body = fs.readFile(filepath, function (err, data) {
+        if (err) { console.log(err); return; }
+        var s3obj = new AWS.S3();
+        var params = { Bucket: '3d-plasticity', Key: filename, ACL: 'public-read', Body: data, Expires: 3600 }
+        s3obj.putObject(params, function (resp) {
+            
+        });
+    });
+}
+
+function getFileFromAws(fileKey, downloadPath) {
+    var s3 = new AWS.S3();
+    var params = { Bucket: '3d-plasticity', Key: fileKey }
+    var file = fs.createWriteStream(downloadPath);
+    var s3file = s3.getObject(params).createReadStream().pipe(file);
 }
