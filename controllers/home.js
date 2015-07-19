391dfd24cb405e8811f9daf7a5834abf6e227b79
Chris Crowell <crowelch@mail.uc.edu>
2015-07-19 10:06:07 -0400

    cleanup home controller logic

--- controllers/home.js
+++ controllers/home.js
@@ -10,71 +10,78 @@ var AWSUrl = 'https://s3.amazonaws.com/3d-plasticity/';
 
 
 exports.upload = function (req, res) {
-    res.render('upload', {
-        title: 'Upload'
-    });
+	res.render('upload', {
+		title: 'Upload'
+	});
 };
 
 exports.postUpload = function (req, res, next) {
-    uploadFileToAWS(req.files.filename.path, req.files.filename.originalname, req.user, function (err, filename) {
-        if(err){
-            return console.log(err);
-        }
-        req.session.filename = filename;
-        req.session.stlfile = "./uploads/" + req.files.filename.name;
-        res.redirect('/viewer');
-    });
+	uploadFileToAWS(req.files.filename.path,
+		req.files.filename.originalname,
+		req.user, function (err, filename) {
+			if(err){
+				return console.log(err);
+			}
+			req.session.filename = filename;
+			req.session.stlfile = "./uploads/" + req.files.filename.name;
+			res.redirect('/viewer');
+		});
 
 }
 
-function uploadFileToAWS(filepath, filename, user,callback) {
+function uploadFileToAWS(filepath, filename, user, callback) {
+	var body = fs.readFile(filepath, function (err, data) {
+		if (err) { console.log(err); return callback(err);}
+		var s3obj = new AWS.S3();
+		constructFilename(user, filename, function(completedFile) {
+			var params = { Bucket: '3d-plasticity', Key: completedFile , ACL: 'public-read', Body: data, Expires: 3600 }
+			s3obj.putObject(params, function (resp) {
+				console.log(resp);
+				callback(null, AWSUrl + completedFile);
+			});
+		});
+	});
+}
 
-    var body = fs.readFile(filepath, function (err, data) {
-        if (err) { console.log(err); return callback(err);}
-        var s3obj = new AWS.S3();
-        var now = Date.now();
-        var completedFile;
-        if (user) {
-            var email = user.email;
-            var truncatedEmail = email.split("@");
-            completedFile = truncatedEmail[0] + "-" + now + "-" + filename;
-            User.findById(user.id, function (err, user) {
-                if (err) {
-                    return console.log(err);
-                }
-                var uploadedFile = new File({
-                    fileName: filename,
-                    s3Url: AWSUrl + completedFile
-                });
-                user.accountHistory.uploadedFiles.push(uploadedFile);
-                user.save(function (err) {
-                    return console.log(err);
-                });
-            });
-        }else {
-            completedFile = "-" + now + "-" + filename;
-        }
-        console.log(completedFile);
-        var params = { Bucket: '3d-plasticity', Key: completedFile , ACL: 'public-read', Body: data, Expires: 3600 }
-        s3obj.putObject(params, function (resp) {
-            callback(null, AWSUrl + completedFile);
-        });
-    });
+function constructFilename(user, filename, cb) {
+	var completedFileName;
+	if (user) {
+		var email = user.email;
+		var truncatedEmail = email.split("@");
+		completedFileName = truncatedEmail[0] + "-" + now + "-" + filename;
+		User.findById(user.id, function (err, user) {
+			if (err) {
+				return console.log(err);
+			}
+			var uploadedFile = new File({
+				fileName: filename,
+				s3Url: AWSUrl + completedFile
+			});
+			user.accountHistory.uploadedFiles.push(uploadedFile);
+			user.save(function (err) {
+				return console.log(err);
+			});
+		});
+	} else {
+		completedFileName = "-" + Date.now() + "-" + filename;
+	}
+	console.log(completedFileName);
+	cb(completedFileName);
 }
 
 function getFileFromAws(fileKey, downloadPath) {
-    var s3 = new AWS.S3();
-    var params = { Bucket: '3d-plasticity', Key: fileKey }
-    var file = fs.createWriteStream(downloadPath);
-    var s3file = s3.getObject(params).createReadStream().pipe(file);
+	var s3 = new AWS.S3();
+	var params = { Bucket: '3d-plasticity', Key: fileKey }
+	var file = fs.createWriteStream(downloadPath);
+	var s3file = s3.getObject(params).createReadStream().pipe(file);
 }
 
 /**
  * GET /
  * Home page.
  */
-exports.index = function(req, res) {
-  res.render('home', {
-    title: 'Home'
-  });
-};
+ exports.index = function(req, res) {
+ 	res.render('home', {
+ 		title: 'Home'
+ 	});
+ };
