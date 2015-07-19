82f59e7177a074691109327e08f248fe1bb34b16
Kenneth Fechter <kenneth.fechter@gmail.com>
2015-07-19 00:35:07 -0400

    redirect users to login, remove social login pages.

--- controllers/user.js
+++ controllers/user.js
@@ -99,7 +99,7 @@ exports.postSignup = function(req, res, next) {
       if (err) return next(err);
       req.logIn(user, function(err) {
         if (err) return next(err);
-        res.redirect('/');
+        res.redirect('/account');
       });
     });
   });
